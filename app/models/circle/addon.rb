a3c8d8d9a5570d031b055e8e31cc59c6986035de
itowtips <ito@web-tips.co.jp>
2014-10-29 20:57:36 +0900

    fix: circle

--- app/models/circle/addon.rb
+++ app/models/circle/addon.rb
@@ -53,8 +53,8 @@ module Circle::Addon
     included do
       field :image_alt, type: String
       field :image_comment, type: String
-      field :image_thumb_width, type: Integer, default: 260
-      field :image_thumb_height, type: Integer, default: 120
+      field :image_thumb_width, type: Integer, default: 120
+      field :image_thumb_height, type: Integer, default: 90
 
       permit_params :image_alt, :image_comment, :image_thumb_width, :image_thumb_height
     end
