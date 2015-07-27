03fd94f46e85f11c560a74be4980fbb669acc462
itowtips <ito@web-tips.co.jp>
2015-07-27 14:42:09 +0900

    [add] facility image's default thumb_size setting

--- app/models/concerns/facility/addon/image_info.rb
+++ app/models/concerns/facility/addon/image_info.rb
@@ -6,8 +6,8 @@ module Facility::Addon
     included do
       field :image_alt, type: String
       field :image_comment, type: String
-      field :image_thumb_width, type: Integer, default: 120
-      field :image_thumb_height, type: Integer, default: 90
+      field :image_thumb_width, type: Integer, default: SS.config.facility.thumb_size[0]
+      field :image_thumb_height, type: Integer, default: SS.config.facility.thumb_size[1]
 
       permit_params :image_alt, :image_comment, :image_thumb_width, :image_thumb_height
     end
