6c4c3fb81ff9d018113f2b0932a129ec03b31a88
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-02-29 18:56:37 +0900

    [bumpup] mail magazine for member

--- app/helpers/member/mypage_helper.rb
+++ app/helpers/member/mypage_helper.rb
@@ -12,4 +12,40 @@ module Member::MypageHelper
     h << %(</nav>)
     h.join
   end
+
+  # def required_label
+  #   %(<span class="required-label">#{t "member.view.required"}</span>).html_safe
+  # end
+
+  def example(key, html_wrap = true)
+    modelnames = @model.ancestors.select { |x| x.respond_to?(:model_name) }
+    msg = ""
+    modelnames.each do |modelname|
+      msg = I18n.t("examples.#{modelname.model_name.i18n_key}.#{key}", default: "")
+      break if msg.present?
+    end
+    return msg if msg.blank? || !html_wrap
+    msg = msg.join(", ") if msg.respond_to?(:join)
+    %(<span class="example">#{msg}</span>).html_safe
+  end
+
+  def remarks(key, html_wrap = true)
+    modelnames = @model.ancestors.select { |x| x.respond_to?(:model_name) }
+    msg = ""
+    modelnames.each do |modelname|
+      msg = I18n.t("remarks.#{modelname.model_name.i18n_key}.#{key}", default: "")
+      break if msg.present?
+    end
+    return msg if msg.blank? || !html_wrap
+    msg = [msg] if msg.class.to_s == "String"
+    list = msg.map {|d| "<li>" + d.to_s.gsub(/\r\n|\n/, "<br />") + "<br /></li>"}
+
+    h  = []
+    h << %(<div class="remarks">)
+    h << %(<ul>)
+    h << list
+    h << %(</ul>)
+    h << %(</div>)
+    h.join("\n").html_safe
+  end
 end
