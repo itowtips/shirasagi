19e68ae80a5be5001f89433eed8e0c7e4e03627a
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-03-04 16:10:56 +0900

    [tweak] map zoom level setting

--- app/models/concerns/board/addon/map_setting.rb
+++ app/models/concerns/board/addon/map_setting.rb
@@ -5,14 +5,14 @@ module Board::Addon
 
     included do
       field :map_state, type: String
-      field :map_zoom_level, type: Integer, default: 10
+      field :map_zoom_level, type: Integer
       field :map_center, type: Map::Extensions::Loc, default: ->{ SS.config.cms.map_center }
 
       permit_params :map_state, :map_zoom_level
       permit_params map_center: [ :lat, :lng ]
 
       validates :map_state, inclusion: { in: %w(enabled disabled) }, if: ->{ gpf_state.present? }
-      validates :map_zoom_level, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 18 }
+      validates :map_zoom_level, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 18, allow_nil: true }
     end
 
     def map_state_options
