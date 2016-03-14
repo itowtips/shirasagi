d32aea29e38af3967f1cb88a7d9e1a966b328554
itowtips <ito@web-tips.co.jp>
2016-03-14 17:06:22 +0900

    [fix] member photo relation file

--- app/models/concerns/member/addon/photo/body.rb
+++ app/models/concerns/member/addon/photo/body.rb
@@ -9,11 +9,20 @@ module Member::Addon::Photo
       field :caption, type: String
 
       permit_params :caption, :image_id, :loc
+
       validate :validate_image
+      validate :validate_in_image
+
+      after_save :update_relation_image_member
     end
 
     def validate_image
       errors.add :image, :empty if !image && !in_image
+    end
+
+    def update_relation_image_member
+      return unless member
+      image.update_attributes(member_id: member.id)
     end
   end
 end
