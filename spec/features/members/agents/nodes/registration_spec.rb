68863fe474c77c4806a08c017d680cc819353869
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-03-18 10:41:06 +0900

    [modify] add spec to confirm successfully logged in

--- spec/features/members/agents/nodes/registration_spec.rb
+++ spec/features/members/agents/nodes/registration_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'members/agents/nodes/registration', type: :feature, dbscope: :example do
   let(:site) { cms_site }
-  let(:node_mypage) { create :member_node_mypage, site: cms_site }
+  let!(:node_mypage) { create :member_node_mypage, site: site, html: '<div id="mypage"></div>' }
   let(:reply_upper_text) do
     %w(
       会員登録ありがとうございました。
@@ -13,10 +13,10 @@ describe 'members/agents/nodes/registration', type: :feature, dbscope: :example 
       ログインパスワードの再設定用のURLをお送りします。
       次の URL をクリックし、画面の指示にしたがってパスワード再設定を完了させてください。).join("\n")
   end
-  let(:node_registration) do
+  let!(:node_registration) do
     create(
       :member_node_registration,
-      cur_site: cms_site,
+      cur_site: site,
       sender_name: '会員登録',
       sender_email: 'admin@example.jp',
       subject: '登録確認',
@@ -27,6 +27,15 @@ describe 'members/agents/nodes/registration', type: :feature, dbscope: :example 
       reset_password_upper_text: reset_password_upper_text,
       reset_password_lower_text: "本メールに心当たりのない方は、お手数ですがメールを削除してください。",
       reset_password_signature: "----\nシラサギ市")
+  end
+  let!(:node_login) do
+    create(
+      :member_node_login,
+      cur_site: site,
+      redirect_url: node_mypage.url,
+      form_auth: "enabled",
+      twitter_oauth: "disabled",
+      facebook_oauth: "disabled")
   end
   let(:index_path) { node_registration.full_url }
 
@@ -48,6 +57,7 @@ describe 'members/agents/nodes/registration', type: :feature, dbscope: :example 
     let(:sex) { "male" }
     let(:era) { "西暦" }
     let(:birthday) { Date.parse("1985-01-01") }
+    let(:password) { "abc123" }
 
     it do
       visit index_path
@@ -95,11 +105,47 @@ describe 'members/agents/nodes/registration', type: :feature, dbscope: :example 
       member = Cms::Member.where(email: email).first
       expect(member.name).to eq name
       expect(member.email).to eq email
+      expect(member.state).to eq "temporary"
       expect(member.kana).to eq kana
       expect(member.tel).to eq tel
       expect(member.addr).to eq addr
       expect(member.sex).to eq sex
       expect(member.birthday).to eq birthday
+
+      mail.body.raw_source =~ /(#{Regexp.escape(node_registration.full_url)}[^ \t\r\n]+)/
+      url = $1
+      expect(url).not_to be_nil
+      visit url
+
+      within "form" do
+        expect(page).to have_css(".colum dd", text: name)
+        expect(page).to have_css(".colum dd", text: email)
+        fill_in "item[in_password]", with: password
+        fill_in "item[in_password_again]", with: password
+
+        click_button "登録"
+      end
+
+      member = Cms::Member.where(email: email).first
+      expect(member.name).to eq name
+      expect(member.email).to eq email
+      expect(member.state).to eq "enabled"
+      expect(member.kana).to eq kana
+      expect(member.tel).to eq tel
+      expect(member.addr).to eq addr
+      expect(member.sex).to eq sex
+      expect(member.birthday).to eq birthday
+
+      click_link "ログイン"
+
+      within "form" do
+        fill_in "item[email]", with: email
+        fill_in "item[password]", with: password
+
+        click_button "ログイン"
+      end
+
+      expect(page). to have_css("div#mypage")
     end
   end
 
