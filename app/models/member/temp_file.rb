dbcd6351706edd17d7a51d59f1a9f8e7b34a42fd
root <root@devss.devss.web-tips.co.jp>
2016-03-25 13:32:24 +0900

    [fix] disable member temp file #validate_member_filesize

--- app/models/member/temp_file.rb
+++ app/models/member/temp_file.rb
@@ -6,12 +6,12 @@ class Member::TempFile
 
   default_scope ->{ where(model: "member/temp_file") }
 
-  validate :validate_member_filesize, if: ->{ in_file.present? && in_files.blank? }
+  #validate :validate_member_filesize, if: ->{ in_file.present? && in_files.blank? }
 
-  def validate_member_filesize
-    return unless member
-    if (size + Member::File.member(member).sum(&:size)) > (1024 * 1024 * 100)
-      errors.add :base, "全ファイル容量が100MBを超えている為、アップロードできません。"
-    end
-  end
+  #def validate_member_filesize
+  #  return unless member
+  #  if (size + Member::File.member(member).sum(&:size)) > (1024 * 1024 * 100)
+  #    errors.add :base, "全ファイル容量が100MBを超えている為、アップロードできません。"
+  #  end
+  #end
 end
