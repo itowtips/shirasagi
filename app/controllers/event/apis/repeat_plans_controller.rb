dab4492d5907ee331b828a521453f26517a88f6e
itowtips <ito@web-tips.co.jp>
2016-11-28 21:28:49 +0900

    [wip]

--- app/controllers/event/apis/repeat_plans_controller.rb
+++ app/controllers/event/apis/repeat_plans_controller.rb
@@ -7,6 +7,52 @@ class Event::Apis::RepeatPlansController < ApplicationController
     end
 
     def create
-      dump params
+      @repeat_type = params[:repeat_type]
+      @repeat_start = params[:repeat_start]
+      @repeat_end = params[:repeat_end]
+      @interval = params[:interval]
+      @wdays = params[:wdays]
+      @interval = 1
+
+      @repeat_start = Date.parse(@repeat_start)
+      @repeat_end = Date.parse(@repeat_end)
+
+      dates = []
+      range = []
+      plan_dates.each do |d|
+        if range.present? && range.last.tomorrow != d
+          dates << range
+          range = []
+        end
+        range << d
+      end
+      dates << range if range.present?
+      @dates = dates
+    end
+
+  private
+    def plan_dates
+      case @repeat_type
+      when 'daily'
+        daily_dates
+      when 'weekly'
+        weekly_dates
+      when 'monthly'
+        monthly_dates
+      else
+        []
+      end
+    end
+
+    def daily_dates
+      @repeat_start.step(@repeat_end, @interval).to_a
+    end
+
+    def weekly_dates
+      []
+    end
+
+    def monthly_dates
+      []
     end
 end
