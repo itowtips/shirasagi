c9180417ab78a216b24f23fdaac01cdffcee5421
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-01-16 20:51:34 +0900

    [add] job_db login node

---
+++ app/models/concerns/job_db/addon/member/target.rb
@@ -0,0 +1,15 @@
+module JobDb::Addon::Member::Target
+  extend ActiveSupport::Concern
+  extend SS::Addon
+
+  included do
+    field :target_class, type: String
+    permit_params :target_class
+  end
+
+  def target_class_options
+    %w(JobNavi::Member Emigration::Member BankSys::JobSeeker BankSys::CompanyMember).map do |v|
+      [ v.constantize.model_name.human, v ]
+    end
+  end
+end
--- app/models/job_db/initializer.rb
+++ app/models/job_db/initializer.rb
@@ -1,5 +1,9 @@
 module JobDb
   class Initializer
+    # login node
+    Cms::Node.plugin "job_db/login"
+
+    # permissions
     Sys::Role.permission :edit_job_db_members, module_name: "job_db"
     Sys::Role.permission :edit_job_db_companies, module_name: "job_db"
   end
