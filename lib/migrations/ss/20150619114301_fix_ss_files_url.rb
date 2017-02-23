b98207bc19bc97ff4bab4e0b4ba4331af82b3f51
itowtips <ito@web-tips.co.jp>
2017-02-23 10:17:25 +0900

    [fix] multibyte filename migration

--- lib/migrations/ss/20150619114301_fix_ss_files_url.rb
+++ lib/migrations/ss/20150619114301_fix_ss_files_url.rb
@@ -1,17 +1,34 @@
 class SS::Migration20150619114301
   def change
+    file_ids = SS::File.pluck(:id)
+    file_ids.each do |id|
+      item = SS::File.find(id) rescue nil
+      next if item.nil?
+      #next if item.filename.ascii_only?
+      item.save!
+
+      item.reload
+      item.set(name: item.basename) if item.name.blank?
+    end
+
     Cms::Page.all.each { |item| replace_fs_urls(item) }
+    Cms::Node.all.each { |item| replace_fs_urls(item) }
     Cms::Layout.all.each { |item| replace_fs_urls(item) }
     Cms::Part.all.each { |item| replace_fs_urls(item) }
   end
 
   def replace_fs_urls(item)
     if item.respond_to?(:html) && item.html.present?
+
       item.set(html: gsub_urls(item.html))
     end
 
     if item.respond_to?(:upper_html) && item.upper_html.present?
       item.set(upper_html: gsub_urls(item.upper_html))
+    end
+
+    if item.respond_to?(:loop_html) && item.loop_html.present?
+      item.set(loop_html: gsub_urls(item.loop_html))
     end
 
     if item.respond_to?(:lower_html) && item.lower_html.present?
@@ -20,11 +37,17 @@ class SS::Migration20150619114301
   end
 
   def gsub_urls(html)
-    html.gsub(%r{(="/fs/)(\d+)(/[\w\-]+\.[\w\-.]+")}) do
+    html.gsub(%r{(="/fs/)(\d+)(/[^\/]+\.[\w\-.]+")}) do
       head = $1
       id   = $2
       tail = $3
-      "#{head}#{id.split("").join("/")}/_#{tail}"
+      url  = SS::File.find(id.to_i).url rescue nil
+
+      if url
+        "=\"#{url}\""
+      else
+        "#{head}#{id.split("").join("/")}/_#{tail}"
+      end
     end
   end
 end
