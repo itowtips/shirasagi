b6bb75c585d41b807408756d1276c5384fb9d53d
itowtips <ito@web-tips.co.jp>
2020-09-03 17:26:26 +0900

    [modify] cms/tabs : add loop_html

--- app/models/concerns/cms/addon/tabs.rb
+++ app/models/concerns/cms/addon/tabs.rb
@@ -3,11 +3,18 @@ module Cms::Addon
     extend ActiveSupport::Concern
     extend SS::Addon
 
+    attr_accessor :cur_date
+
     included do
       field :conditions, type: SS::Extensions::Words
       field :limit, type: Integer, default: 8
+      field :loop_html, type: String
       field :new_days, type: Integer, default: 1
+      field :loop_format, type: String
+      field :loop_liquid, type: String
+
       permit_params :conditions, :limit, :new_days
+      permit_params :loop_html, :loop_format, :loop_liquid
 
       before_validation :validate_conditions
     end
@@ -24,6 +31,37 @@ module Cms::Addon
 
     def in_new_days?(date)
       date + new_days > Time.zone.now
+    end
+
+    def loop_format_options
+      %w(shirasagi liquid).map do |v|
+        [ I18n.t("cms.options.loop_format.#{v}"), v ]
+      end
+    end
+
+    def upper_html
+      nil
+    end
+
+    def lower_html
+      nil
+    end
+
+    def loop_format_liquid?
+      loop_format == "liquid"
+    end
+
+    def loop_format_shirasagi?
+      !loop_format_liquid?
+    end
+
+    def loop_setting
+      nil
+    end
+
+    def render_loop_html(item, opts = {})
+      item = item.becomes_with_route rescue item
+      item.render_template(opts[:html] || loop_html, self)
     end
 
     private
