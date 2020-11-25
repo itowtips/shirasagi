7d543cf4083cf9a03187df2eb5414950fb81d35b
itowtips <ito@web-tips.co.jp>
2020-11-25 11:00:14 +0900

    [modify] Tourism::Addon::Facility : liquidize facility

--- app/models/concerns/tourism/addon/facility.rb
+++ app/models/concerns/tourism/addon/facility.rb
@@ -7,9 +7,19 @@ module Tourism::Addon
       belongs_to :facility, class_name: "Facility::Node::Page"
       permit_params :facility_id
       validates :facility_id, presence: true
+
+      template_variable_handler(:facility_name, :template_variable_handler_facility_name)
+
+      liquidize do
+        export :facility
+      end
     end
 
     private
+
+    def template_variable_handler_facility_name(name, issuer)
+      facility.try(:name)
+    end
 
     def template_variable_handler_class_categories(name, issuer)
       labels = []
