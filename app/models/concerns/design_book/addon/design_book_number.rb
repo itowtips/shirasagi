c412a447c1876a312adec5468602bdfa7a3b9d6f
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-11-25 20:57:36 +0900

    [fix] copy design_book page (#71)

--- app/models/concerns/design_book/addon/design_book_number.rb
+++ app/models/concerns/design_book/addon/design_book_number.rb
@@ -6,8 +6,9 @@ module DesignBook::Addon
     included do
       field :design_book_number, type: String
       permit_params :design_book_number
-      validates :design_book_number, uniqueness: { scope: :site_id }, length: { is: 10, allow_blank: true },
+      validates :design_book_number, length: { is: 10, allow_blank: true },
                 numericality: { only_integer: true, allow_blank: true }
+      validates :design_book_number, presence: true, if: -> { master? }
 
       if respond_to? :template_variable_handler
         template_variable_handler :design_book_number, :template_variable_handler_name
@@ -20,7 +21,7 @@ module DesignBook::Addon
     end
 
     def set_filename
-      self.basename = design_book_number
+      self.basename = design_book_number + fix_extname if cur_node.present? && master?
       super
     end
   end
