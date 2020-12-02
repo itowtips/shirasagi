1d68ca5b249101d01cc5accb804ecdf48661a254
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-12-02 20:15:42 +0900

    [modify] contact addon group contact_group_name (#78)

--- app/models/concerns/contact/addon/group.rb
+++ app/models/concerns/contact/addon/group.rb
@@ -4,12 +4,13 @@ module Contact::Addon
     extend SS::Addon
 
     included do
+      field :contact_group_name, type: String
       field :contact_tel, type: String
       field :contact_fax, type: String
       field :contact_email, type: String
       field :contact_link_url, type: String
       field :contact_link_name, type: String
-      permit_params :contact_tel, :contact_fax, :contact_email
+      permit_params :contact_group_name, :contact_tel, :contact_fax, :contact_email
       permit_params :contact_link_url, :contact_link_name
     end
   end
