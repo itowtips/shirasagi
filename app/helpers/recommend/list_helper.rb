842f639bd11e2326345baef08643eca927440992
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-12-07 17:26:58 +0900

    [modify] recommend list_helper render_content_list (#85)

--- app/helpers/recommend/list_helper.rb
+++ app/helpers/recommend/list_helper.rb
@@ -15,16 +15,37 @@ module Recommend::ListHelper
     displayed = 0
     @items.each do |item|
       next if display_list.index(item.path)
+      next if display_list.index(item.access_url)
       content = item.content
       next unless content
       next unless content.public?
 
-      display_list << item.path
+      if @cur_site.inquiry_form.try(:filename) == content.filename
+        display_list << item.access_url
+      else
+        display_list << item.path
+      end
       displayed += 1
       if cur_item.loop_setting.present?
         ih = item.render_template(cur_item.loop_setting.html, self)
       elsif cur_item.loop_html.present?
-        ih = cur_item.render_loop_html(content)
+        if @cur_site.inquiry_form.try(:filename) == content.filename
+          uri = Addressable::URI.parse(item.access_url)
+          query = Rack::Utils.parse_nested_query(uri.query)
+          group = Cms::Group.where(id: query['group'].to_s).first
+          html = cur_item.loop_html.gsub('#{url}', item.access_url)
+          if group.present?
+            if group.contact_group_name.present?
+              group_name = group.contact_group_name + " " + content.name
+            else
+              group_name = group.section_name + " " + content.name
+            end
+            html = html.gsub('#{name}', group_name)
+          end
+        else
+          html = cur_item.loop_html
+        end
+        ih = cur_item.render_loop_html(content, html: html)
       else
         ih = []
         ih << '<article class="item-#{class}">'
