3d34ae89aacace46c3ae7f9a7eda9a5878312234
itowtips <ito@web-tips.co.jp>
2021-10-11 19:39:21 +0900

    [modify] line service : my_plan

--- app/models/cms/line/service/my_plan.rb
+++ app/models/cms/line/service/my_plan.rb
@@ -1,97 +1,6 @@
 class Cms::Line::Service::MyPlan < Cms::Line::Service::Base
   include Cms::Addon::Line::Service::PageList
 
-  def flex_carousel_template(title, items)
-    items = [items] if !items.is_a?(Array)
-
-    contents = items.map do |item|
-      opts = OpenStruct.new
-      yield(item, opts)
-
-      image = opts[:image]
-      image_url = opts[:image_url]
-      name = opts[:name].to_s
-      text = opts[:text].to_s
-      action = opts[:action]
-
-      content = { type: "bubble", size: "kilo" }
-
-      if image
-        content[:hero] = {
-          type: "image",
-          url: image.full_url,
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover"
-        }
-      elsif image_url
-        content[:hero] = {
-          type: "image",
-          url: image_url,
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover"
-        }
-      end
-
-      content[:body] = {
-          type: "box",
-          layout: "vertical",
-          contents: []
-      }
-
-      # name
-      content[:body][:contents] << {
-          type: "text",
-          text: name,
-          wrap: true,
-          weight: "bold",
-          margin: "none"
-      }
-
-      # text
-      text.split("\n").each_with_index do |line, idx|
-        content[:body][:contents] << {
-            type: "text",
-            text: line,
-            wrap: true,
-            size: "sm",
-            margin: (idx == 0) ? "md" : "none"
-        }
-      end
-
-      # action
-      if action
-        content[:footer] = {
-            type: "box",
-            layout: "vertical",
-            contents: [
-                {
-                    type: "button",
-                    action: action,
-                    style: "secondary",
-                    margin: "none"
-                }
-            ]
-        }
-        content[:styles] = {
-            footer: { separator: true }
-        }
-      end
-
-      content
-    end
-
-    {
-        type: "flex",
-        altText: title,
-        contents: {
-            type: "carousel",
-            contents: contents
-        }
-    }
-  end
-
   def switch_messages
     date = Time.zone.now.change(hour: 0, min: 0, sec: 0)
     items = Cms::Page.public_list(site: site, parent: self).in(event_dates: date).to_a
@@ -99,10 +8,8 @@ class Cms::Line::Service::MyPlan < Cms::Line::Service::Base
     items = items.take(10)
 
     if items.present?
-      template = flex_carousel_template("今日の予定", items) do |item, opts|
-        image_url = nil
-        image_url = no_image.full_url if no_image
-
+      template = Cms::LineUtils.flex_carousel_template("今日の予定", items) do |item, opts|
+        image_url = no_image.try(:full_url)
         if item.thumb
           image_url = item.thumb.full_url
         else
