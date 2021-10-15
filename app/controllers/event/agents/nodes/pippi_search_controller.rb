1495bf590ba2cdc43ed5d3c26fa248b010b4af93
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2021-10-15 19:24:29 +0900

    [modify] event agents nodes pippi_search_controller list_events (#12)

--- app/controllers/event/agents/nodes/pippi_search_controller.rb
+++ app/controllers/event/agents/nodes/pippi_search_controller.rb
@@ -8,12 +8,8 @@ class Event::Agents::Nodes::PippiSearchController < ApplicationController
   before_action :set_params
 
   def index
-    @categories = []
     @items = []
-    if @cur_node.parent
-      @categories = Cms::Node.site(@cur_site).where({:id.in => @cur_node.parent.st_category_ids}).sort(filename: 1)
-    end
-    if @keyword.present? || @category_ids.present? || @start_date.present? || @close_date.present? || @facility_ids.present?
+    if @keyword.present? || @location_id.present? || @facility_id.present? || @genre_ids.present? || @age_ids.present?
       list_events
     end
   end
@@ -27,9 +23,9 @@ class Event::Agents::Nodes::PippiSearchController < ApplicationController
   def set_params
     safe_params = params.permit(:search_keyword, :location_id, :facility_id, genre_ids: [], age_ids: [])
     @keyword = safe_params[:search_keyword].presence
-    @location_id = safe_params[:location_id].presence
+    @location_id = safe_params[:location_id].to_i.nonzero?
     @location = @event_node.st_locations.site(@cur_site).and_public.where(id: @location_id).first
-    @facility_id = safe_params[:facility_id].presence
+    @facility_id = safe_params[:facility_id].to_i.nonzero?
     @facility = @event_node.st_facilities.site(@cur_site).and_public.where(id: @facility_id).first
     @genre_ids = safe_params[:genre_ids].presence || []
     @genre_ids = @genre_ids.map(&:to_i)
@@ -43,18 +39,11 @@ class Event::Agents::Nodes::PippiSearchController < ApplicationController
     criteria = Cms::Page.site(@cur_site).and_public
     criteria = criteria.search(keyword: @keyword) if @keyword.present?
     criteria = criteria.where(@cur_node.condition_hash)
-    criteria = criteria.in(category_ids: @category_ids) if @category_ids.present?
-    criteria = criteria.in(facility_ids: @facility_ids) if @facility_name.present?
-
-    if @start_date.present? && @close_date.present?
-      criteria = criteria.search(dates: @start_date..@close_date)
-    elsif @start_date.present?
-      criteria = criteria.search(start_date: @start_date)
-    elsif @close_date.present?
-      criteria = criteria.search(close_date: @close_date)
-    else
-      criteria = criteria.exists(event_dates: 1)
-    end
+    criteria = criteria.exists(event_dates: 1)
+    category_ids = [@location_id] + @genre_ids + @age_ids
+    category_ids = category_ids.uniq.compact
+    criteria = criteria.in(category_ids: category_ids) if category_ids.present?
+    criteria = criteria.where(facility_ids: @facility_id) if @facility_id.present?
 
     @items = criteria.order_by(@cur_node.sort_hash).
       page(params[:page]).
